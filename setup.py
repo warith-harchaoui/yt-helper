31f0200cf1e50ccada71632c1ea83bb76c8f94e4
Warith HARCHAOUI <warith.harchaoui@gmail.com>
2026-06-23 03:21:50 +0200

    chore: bump os-helper pin to v1.1.0, release v0.2.2

--- setup.py
+++ setup.py
@@ -14,13 +14,13 @@ package_data = \
 install_requires = \
 ['audio-helper @ git+https://github.com/warith-harchaoui/audio-helper.git@v1.1.0',
  'ffmpeg-python>=0.2.0,<0.3.0',
- 'os-helper @ git+https://github.com/warith-harchaoui/os-helper.git@v1.0.0',
+ 'os-helper @ git+https://github.com/warith-harchaoui/os-helper.git@v1.1.0',
  'video-helper @ git+https://github.com/warith-harchaoui/video-helper.git@v1.0.0',
  'yt-dlp>=2024.12.23,<2025.0.0']
 
 setup_kwargs = {
     'name': 'yt-helper',
-    'version': '0.2.1',
+    'version': '0.2.2',
     'description': 'YT Helper is a Python library that provides utility functions for downloading videos, audio, and thumbnails from platforms like YouTube, Vimeo, and DailyMotion using yt-dlp. It also supports post-processing tasks such as converting or merging media files with ffmpeg.',
     'long_description': long_description,
     'long_description_content_type': 'text/markdown',
